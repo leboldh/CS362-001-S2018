1bd6568e09518be3bf1cf55969f31809475d93a4
Haley Lebold <leboldh@flip2.engr.oregonstate.edu>
2018-06-09 19:16:58 -0700

    finalized tests for URLValidator

--- projects/leboldh/Final-Project-B/URLValidatorInCorrect/src/test/java/finalprojectB/UrlValidatorTest.java
+++ projects/leboldh/Final-Project-B/URLValidatorInCorrect/src/test/java/finalprojectB/UrlValidatorTest.java
@@ -23,120 +23,120 @@ public class UrlValidatorTest extends TestCase {
       super(testName);
    }
 
-//   public void testManualTestF1()
-//   {
-//    UrlValidator urlVal = new UrlValidator();
-//    assertTrue(urlVal.isValid("http://www.google.com"));
-//   }
-//
-//   public void testManualTestF2()
-//   {
-//     UrlValidator urlValAll = new UrlValidator(UrlValidator.ALLOW_ALL_SCHEMES);
-//     assertTrue(urlValAll.isValid("https://www.google.com"));
-//   }
-//
-//   public void testManualTestF3()
-//   {
-//     UrlValidator urlValAll = new UrlValidator(UrlValidator.ALLOW_ALL_SCHEMES);
-//     assertTrue(urlValAll.isValid("ftp://www.google.com:40/test1?action=view#bar"));
-//   }
-//
-//   public void testManualTestF4()
-//   {
-//     UrlValidator urlValAll = new UrlValidator(UrlValidator.ALLOW_ALL_SCHEMES);
-//     assertTrue(urlValAll.isValid("hahaha://www.google.com"));
-//   }
-//
-//   public void testManualTestF5()
-//   {
-//     UrlValidator urlValAll = new UrlValidator(UrlValidator.NO_FRAGMENTS);
-//     assertTrue(urlValAll.isValid("http://www.google.com"));
-//   }
-//
-//   public void testManualTestP1()
-//   {
-//     String testString = null;
-//
-//     UrlValidator urlVal = new UrlValidator();
-//     assertFalse(urlVal.isValid("hahaha://www.google.com"));
-//     assertFalse(urlVal.isValid(testString));
-//     assertFalse(urlVal.isValid(""));
-//     assertFalse(urlVal.isValid("7"));
-//   }
-//
-//   public void testManualTestP2()
-//   {
-//     String testString = null;
-//
-//     UrlValidator urlValAll = new UrlValidator(UrlValidator.ALLOW_ALL_SCHEMES);
-//     assertTrue(urlValAll.isValid("http://www.google.com"));
-//     assertFalse(urlValAll.isValid(testString));
-//     assertFalse(urlValAll.isValid(""));
-//     assertFalse(urlValAll.isValid("7"));
-//   }
-//
-//   public void testManualTestP3()
-//   {
-//     String testString = null;
-//     UrlValidator urlValAll = new UrlValidator(UrlValidator.NO_FRAGMENTS);
-//     assertFalse(urlValAll.isValid("ftp://www.google.com:40/test1?action=view#bar"));
-//     assertFalse(urlValAll.isValid(testString));
-//     assertFalse(urlValAll.isValid(""));
-//     assertFalse(urlValAll.isValid("7"));
-//   }
-//
-//   public void testYourFirstPartitionF1()
-//   {
-//     //Partition 1: simple URLs without port, path, query, or fragment
-//     UrlValidator urlValAll = new UrlValidator(UrlValidator.ALLOW_ALL_SCHEMES);
-//     assertFalse(urlValAll.isValid("http://"));
-//   }
-//
-//   public void testYourFirstPartitionF2()
-//   {
-//     //Partition 1: simple URLs without port, path, query, or fragment
-//     String[] CUST_SCHEMES = {"http", "https", "file", "test"};
-//     UrlValidator urlValCust = new UrlValidator(CUST_SCHEMES);
-//     assertTrue(urlValCust.isValid("http://www.google.com"));
-//   }
-//
-//   public void testYourFirstPartitionF3()
-//   {
-//     //Partition 1: simple URLs without port, path, query, or fragment
-//     String[] CUST_SCHEMES = {"http", "https", "file", "test"};
-//     UrlValidator urlValCust = new UrlValidator(CUST_SCHEMES);
-//     assertTrue(urlValCust.isValid("test://www.google.com"));
-//   }
-//
-//   public void testYourFirstPartitionF4()
-//   {
-//     //Partition 1: simple URLs without port, path, query, or fragment
-//     UrlValidator urlValAll = new UrlValidator(UrlValidator.ALLOW_ALL_SCHEMES);
-//     assertFalse(urlValAll.isValid("http://aaa"));
-//   }
-//
-// public void testYourFirstPartition()
-// {
-//   //Partition 1: simple URLs without port, path, query, or fragment
-//   UrlValidator urlValAll = new UrlValidator(UrlValidator.ALLOW_ALL_SCHEMES);
-//   assertTrue(urlValAll.isValid("http://www.google.com"));
-// }
-//
-//   public void testYourSecondPartitionF1(){
-//     //Partition 2: complex URLs - include at least one of port, path, query, or fragment
-//     UrlValidator urlValAll = new UrlValidator(UrlValidator.ALLOW_ALL_SCHEMES);
-//     assertTrue(urlValAll.isValid("http://www.google.com:40/test1?action=view#bar"));
-//   }
-//
-//   public void testYourSecondPartition(){
-//     //Partition 2: complex URLs - include at least one of port, path, query, or fragment
-//     UrlValidator urlValAll = new UrlValidator(UrlValidator.ALLOW_ALL_SCHEMES);
-//     assertFalse(urlValAll.isValid("http://www.google.com:-1/test1?action=view#bar"));
-//
-//     UrlValidator urlValNF = new UrlValidator(UrlValidator.NO_FRAGMENTS);
-//     assertFalse(urlValNF.isValid("http://www.google.com:40/test1?action=view#bar"));
-//   }
-//
+   public void testManualTestF1()
+   {
+    UrlValidator urlVal = new UrlValidator();
+    assertTrue(urlVal.isValid("http://www.google.com"));
+   }
+
+   public void testManualTestF2()
+   {
+     UrlValidator urlValAll = new UrlValidator(UrlValidator.ALLOW_ALL_SCHEMES);
+     assertTrue(urlValAll.isValid("https://www.google.com"));
+   }
+
+   public void testManualTestF3()
+   {
+     UrlValidator urlValAll = new UrlValidator(UrlValidator.ALLOW_ALL_SCHEMES);
+     assertTrue(urlValAll.isValid("ftp://www.google.com:40/test1?action=view#bar"));
+   }
+
+   public void testManualTestF4()
+   {
+     UrlValidator urlValAll = new UrlValidator(UrlValidator.ALLOW_ALL_SCHEMES);
+     assertTrue(urlValAll.isValid("hahaha://www.google.com"));
+   }
+
+   public void testManualTestF5()
+   {
+     UrlValidator urlValAll = new UrlValidator(UrlValidator.NO_FRAGMENTS);
+     assertTrue(urlValAll.isValid("http://www.google.com"));
+   }
+
+   public void testManualTestP1()
+   {
+     String testString = null;
+
+     UrlValidator urlVal = new UrlValidator();
+     assertFalse(urlVal.isValid("hahaha://www.google.com"));
+     assertFalse(urlVal.isValid(testString));
+     assertFalse(urlVal.isValid(""));
+     assertFalse(urlVal.isValid("7"));
+   }
+
+   public void testManualTestP2()
+   {
+     String testString = null;
+
+     UrlValidator urlValAll = new UrlValidator(UrlValidator.ALLOW_ALL_SCHEMES);
+     assertTrue(urlValAll.isValid("http://www.google.com"));
+     assertFalse(urlValAll.isValid(testString));
+     assertFalse(urlValAll.isValid(""));
+     assertFalse(urlValAll.isValid("7"));
+   }
+
+   public void testManualTestP3()
+   {
+     String testString = null;
+     UrlValidator urlValAll = new UrlValidator(UrlValidator.NO_FRAGMENTS);
+     assertFalse(urlValAll.isValid("ftp://www.google.com:40/test1?action=view#bar"));
+     assertFalse(urlValAll.isValid(testString));
+     assertFalse(urlValAll.isValid(""));
+     assertFalse(urlValAll.isValid("7"));
+   }
+
+   public void testYourFirstPartitionF1()
+   {
+     //Partition 1: simple URLs without port, path, query, or fragment
+     UrlValidator urlValAll = new UrlValidator(UrlValidator.ALLOW_ALL_SCHEMES);
+     assertFalse(urlValAll.isValid("http://"));
+   }
+
+   public void testYourFirstPartitionF2()
+   {
+     //Partition 1: simple URLs without port, path, query, or fragment
+     String[] CUST_SCHEMES = {"http", "https", "file", "test"};
+     UrlValidator urlValCust = new UrlValidator(CUST_SCHEMES);
+     assertTrue(urlValCust.isValid("http://www.google.com"));
+   }
+
+   public void testYourFirstPartitionF3()
+   {
+     //Partition 1: simple URLs without port, path, query, or fragment
+     String[] CUST_SCHEMES = {"http", "https", "file", "test"};
+     UrlValidator urlValCust = new UrlValidator(CUST_SCHEMES);
+     assertTrue(urlValCust.isValid("test://www.google.com"));
+   }
+
+   public void testYourFirstPartitionF4()
+   {
+     //Partition 1: simple URLs without port, path, query, or fragment
+     UrlValidator urlValAll = new UrlValidator(UrlValidator.ALLOW_ALL_SCHEMES);
+     assertFalse(urlValAll.isValid("http://aaa"));
+   }
+
+ public void testYourFirstPartition()
+ {
+   //Partition 1: simple URLs without port, path, query, or fragment
+   UrlValidator urlValAll = new UrlValidator(UrlValidator.ALLOW_ALL_SCHEMES);
+   assertTrue(urlValAll.isValid("http://www.google.com"));
+ }
+
+   public void testYourSecondPartitionF1(){
+     //Partition 2: complex URLs - include at least one of port, path, query, or fragment
+     UrlValidator urlValAll = new UrlValidator(UrlValidator.ALLOW_ALL_SCHEMES);
+     assertTrue(urlValAll.isValid("http://www.google.com:40/test1?action=view#bar"));
+   }
+
+   public void testYourSecondPartition(){
+     //Partition 2: complex URLs - include at least one of port, path, query, or fragment
+     UrlValidator urlValAll = new UrlValidator(UrlValidator.ALLOW_ALL_SCHEMES);
+     assertFalse(urlValAll.isValid("http://www.google.com:-1/test1?action=view#bar"));
+
+     UrlValidator urlValNF = new UrlValidator(UrlValidator.NO_FRAGMENTS);
+     assertFalse(urlValNF.isValid("http://www.google.com:40/test1?action=view#bar"));
+   }
+
 
    //Programming based testing
     public static int getRandomIntBetween(Random random, int min, int max) {
@@ -151,7 +151,7 @@ public class UrlValidatorTest extends TestCase {
     public void testScheme(){
         Random random = new Random(Calendar.getInstance().getTimeInMillis());
         int pick = getRandomIntBetween(random, 0,1);
-        //pick = 2;
+        //pick = 1;
 
         switch (pick){
             case 0: //all schemes
@@ -230,7 +230,7 @@ public class UrlValidatorTest extends TestCase {
     public void testAuthority() {
         Random random = new Random(Calendar.getInstance().getTimeInMillis());
         int pick = getRandomIntBetween(random, 0,1);
-        //pick = 1;
+        pick = 0;
 
         if (pick == 0) { //check empty authority
             UrlValidator emptyAuth = new UrlValidator(UrlValidator.ALLOW_ALL_SCHEMES);
